e4dff4a06ff39ccb962f2c084e42b0c1f610c1e1
AhmedRedar55667 <ahmedredaelgahzle140@gmail.com>
2026-06-14 19:32:50 +0300

    Modify Capacitor config for new webDir and server
    Updated webDir to 'dist' and added server configuration.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.hamor.game',
   appName: 'hamor',
-  webDir: 'build/client' // التعديل السحري: وجهنا الكاباسيتور لمجلد Remix الصحيح
+  webDir: 'dist', // هنخليه dist ونخلق الفولدر ده وهمي في السيرفر
+  server: {
+    url: 'https://رابط-لعبتك-الأونلاين-الحقيقي.com', // 👈 ضع هنا رابط اللعبة الأونلاين بتاعكم
+    cleartext: true
+  }
 };
 
 export default config;
