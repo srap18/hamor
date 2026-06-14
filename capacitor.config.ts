532de01d01ef1cd67928ce49afaad56d9131c36f
AhmedRedar55667 <ahmedredaelgahzle140@gmail.com>
2026-06-14 16:49:53 +0300

    Update Capacitor config for app ID and name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.8d4aa159ae7c40e3828c4457d68d9a07',
-  appName: 'fishing-game',
-  webDir: 'dist',
-  server: {
-    url: 'https://8d4aa159-ae7c-40e3-828c-4457d68d9a07.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  appId: 'com.hamor.game',
+  appName: 'hamor',
+  webDir: 'dist'
 };
 
 export default config;
