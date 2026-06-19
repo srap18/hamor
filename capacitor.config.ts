1e1a7f1ce02b700c37b9c185bfcf6da408fee66c
AhmedRedar55667 <ahmedredaelgahzle140@gmail.com>
2026-06-19 13:59:40 +0300

    Modify Capacitor config for app name and plugins
    Updated app name and added plugin configuration for SplashScreen.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.hamor.game',
-  appName: 'ملوك القراصنة', // تم تحديث اسم اللعبة هنا برمجياً لـ Capacitor
+  appName: 'ملوك القراصنة',
   webDir: 'dist',
   server: {
-    // الرابط الرسمي الجديد الصحيح 
     url: 'https://www.molok-alqarasna.com', 
     cleartext: true,
-    // السماح بالتنقل لضمان فتح صفحات الدفع والتسجيل الخارجية دون الخروج من التطبيق
     allowNavigation: [
       'www.molok-alqarasna.com',
       '*.molok-alqarasna.com',
@@ -18,8 +16,15 @@ const config: CapacitorConfig = {
     ]
   },
   android: {
-    // تفعيل المحتوى المختلط لضمان عمل السيرفرات والـ WebSockets الخاصة باللعبة الأونلاين بكفاءة
     allowMixedContent: true
+  },
+  // إضافة هذا الجزء يضمن توافق الأيقونات مع أداة التوليد التي نستخدمها
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#000000",
+    }
   }
 };
 
